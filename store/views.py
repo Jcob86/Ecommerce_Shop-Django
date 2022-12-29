9f8e7e0a5f0ac60f735316aa85c60fe1fe4cb6b7
Jakub Żmudziński <kubaz8645@gmail.com>
2022-12-29 15:15:46 +0100

    Customization in templates view

--- store/views.py
+++ store/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from .models import Collection, Product
 
 
 def main(request):
-    return render(request, "store/main.html")
+    collections = Collection.objects.all()
+    collection_titles = []
+    for collection in collections:
+        collection_titles.append(collection.title)
+    context = {
+        'collection_title':collection_titles
+    }
+    return render(request, "store/main.html", context)
+
+
+class ProductsList(ListView):
+    pass
+
+
+def test(request):
+    return render(request, "store/test.html")
