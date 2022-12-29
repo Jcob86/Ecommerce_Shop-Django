1f0037002f53f54a5afb0be3fcc14f45cd595378
Jakub Żmudziński <kubaz8645@gmail.com>
2022-12-29 02:36:47 +0100

    Core of project and base template

---
+++ store/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from store.validators import validate_file_size
+from django.core.validators import MinValueValidator
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Promotion(models.Model):
+    title = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])
+    price = models.DecimalField(max_digits=9, decimal_places=2, validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, related_name='products')
+    promotions = models.ManyToManyField(Promotion, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
